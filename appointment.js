57d93c38f46031c3de61c4f154cce16e510fdeb1
lasantha96 <lsanjeewa947@gmail.com>
2022-08-19 16:26:47 +0400

    Multi stage added

--- appointment.js
+++ appointment.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello User'),
+      body: JSON.stringify('Hello Appointment'),
   };
   return response;
 };
--- doctor.js
+++ doctor.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello User'),
+      body: JSON.stringify('Hello Doctor'),
   };
   return response;
 };
